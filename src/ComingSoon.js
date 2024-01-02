8a6be0ae8504463f510668645b6d254654d67f98
Vijay <test@test.com>
2024-01-02 07:11:05 -0500

    Most Important Task

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -22,6 +22,16 @@ function ComingSoon() {
         Download Magical Resolution Template
       </a>
 
+      <a
+        href="https://firebasestorage.googleapis.com/v0/b/championkidshub.appspot.com/o/public%2FMostImportantTask.pdf?alt=media&token=abff32fd-326d-4bd4-8699-9c4ce0c3acd4"
+        download="MostImportantTask_ChampionKidsHub.pdf"
+        class="download-button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Download Most Important Task Template
+      </a>
+
       <iframe
         src="https://app.acuityscheduling.com/schedule.php?owner=31096105"
         title="Schedule Appointment"
@@ -33,6 +43,13 @@ function ComingSoon() {
         src="https://embed.acuityscheduling.com/js/embed.js"
         type="text/javascript"
       ></script>
+
+      <footer class="contact-info">
+        <p>
+          Have questions? Get in touch at{" "}
+          <a href="mailto:info@championkidshub.com">info@championkidshub.com</a>
+        </p>
+      </footer>
     </div>
   );
 }
